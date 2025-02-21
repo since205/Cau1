412a9d61546c75f837dbc3416891f71fdfd48480
since205 <142366768+since205@users.noreply.github.com>
2025-02-21 15:20:01 +0700

    second  commit

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,32 @@
+﻿using EmtityWithNet.Data.ett1_web_api.Data;
+using EmtityWithNet.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmtityWithNet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly StudentContext _context;
+
+        public StudentsController(StudentContext context)
+        {
+            _context = context;
+        }
+
+        //action
+        [HttpPost("CreateStudent")]
+        public async Task<IActionResult> CreateStudent(Student student)
+        {
+
+            _context.Students.Add(student);
+            await _context.SaveChangesAsync();
+
+            return Ok(new { message = "Student added successfully", student });
+
+        }
+
+    }
+}
---
+++ Data/StudentContext.cs
@@ -0,0 +1,26 @@
+﻿namespace EmtityWithNet.Data
+{
+
+    using Microsoft.AspNetCore.Hosting.Server;
+    //using System.Data.Entity;//winform
+    using Microsoft.EntityFrameworkCore;
+    using System.Security.Cryptography;
+    using System;
+    using Model;
+    using System.Collections.Generic;
+
+
+    namespace ett1_web_api.Data
+    {
+        public class StudentContext : DbContext
+        {
+            // public StudentContext() : base("name=StudentDBConnectionString") //winform
+            public StudentContext(DbContextOptions<StudentContext> options) : base(options) //webapi
+            {
+            }
+
+            public DbSet<Student> Students { get; set; }
+
+        }
+    }
+}
